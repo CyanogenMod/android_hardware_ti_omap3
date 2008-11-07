32e4bae0311fcfa8d0e4f10a7663d565ab450e64
Keith Deacon <kdeacon@ti.com>
2008-11-06 18:22:55 -0600

    Added license headers in libaudio

--- libaudio/AudioStreamOutOmap.h
+++ libaudio/AudioStreamOutOmap.h
@@ -1,3 +1,19 @@
+/*
+ * AudioStreamOutOmap.h
+ *
+ * Audio output stream handler for OMAP.
+ *
+ * Copyright (C) 2008 Texas Instruments, Inc.
+ *
+ * This package is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #ifndef _AUDIO_STREAM_OUT_OMAP_H_
 #define _AUDIO_STREAM_OUT_OMAP_H_
 
