9d6f8a140cceebc88e93339401e80356d17687e7
SRINI GOSANGI <srini.gosangi@windriver.com>
2009-05-15 09:51:24 -0700

    support for direct rendering into overly buffers non-cacheable buffers

--- libopencorehw/android_surface_output_omap34xx.h
+++ libopencorehw/android_surface_output_omap34xx.h
@@ -1,3 +1,22 @@
+/*
+ * OMAP3430 support
+ *
+ * Author: Srini Gosangi <srini.gosangi@windriver.com>
+ * Author: Michael Barabanov <michael.barabanov@windriver.com>
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the License for the specific language governing permissions
+ */
+
 /* ------------------------------------------------------------------
  * Copyright (C) 2008 PacketVideo
  *
@@ -20,6 +39,7 @@
 #define ANDROID_SURFACE_OUTPUT_OMAP34XXH_INCLUDED
 
 #include "android_surface_output.h"
+#include "buffer_alloc_omap34xx.h"
 
 // support for shared contiguous physical memory
 #include <ui/Overlay.h>
@@ -32,20 +52,24 @@ public:
 
     // frame buffer interface
     virtual bool initCheck();
+    virtual void setParametersSync(PvmiMIOSession aSession, PvmiKvp* aParameters, int num_elements, PvmiKvp * & aRet_kvp);
+    virtual PVMFStatus getParametersSync(PvmiMIOSession aSession, PvmiKeyType aIdentifier,
+        PvmiKvp*& aParameters, int& num_parameter_elements, PvmiCapabilityContext aContext);
     virtual PVMFStatus WriteFrameBuf(uint8* aData, uint32 aDataLen, const PvmiMediaXferHeader& data_header_info);
-    //OSCL_IMPORT_REF virtual PVMFStatus WriteFrameBuf(uint8* aData, uint32 aDataLen, const PvmiMediaXferHeader& data_header_info);
-	//virtual void postLastFrame();
+
     virtual void CloseFrameBuf();
 
     OSCL_IMPORT_REF ~AndroidSurfaceOutputOmap34xx();
 private:
 
+    BufferAllocOmap34xx			                mbufferAlloc;
     bool						mUseOverlay;
     sp<Overlay> 				mOverlay; 
-	int 						mBuffer_count;
-	int 						mRecycle_buffer_count;
-    void*						mOverlay_buffer_address[4];//max buffers supported in overlay is 4
+    int 						bufEnc;
     
+    int32 iNumberOfBuffers;
+    int32 iBufferSize;
+
     #if 0
     class DispatchBufferThread : public Thread {
         AndroidSurfaceOutputOmap34xx* mOmapMIO;
