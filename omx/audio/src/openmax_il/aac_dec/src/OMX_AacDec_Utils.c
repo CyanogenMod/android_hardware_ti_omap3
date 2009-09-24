383331e934246590d855e65e3e543d206aa026bf
Benny Wong <Benny.Wong@motorola.com>
2009-09-23 23:52:31 -0500

    Fix to honor seek request after EOS. bug 2127768
    OMX Component was not reseting first_buff flag when flushing output buffers.
    Whenever the IL client does not flush input buffers the error reported is
    present. Since the EOS has been resolved at the SN level, output buffer
    can reset the firt_buff safely.
    
    Originally from: https://partner.source.android.com/g/#change,1151

--- omx/audio/src/openmax_il/aac_dec/src/OMX_AacDec_Utils.c
+++ omx/audio/src/openmax_il/aac_dec/src/OMX_AacDec_Utils.c
@@ -1724,7 +1724,7 @@ OMX_U32 AACDEC_HandleCommand (AACDEC_COMPONENT_PRIVATE *pComponentPrivate)
         if(commandData == 0x1 || commandData == -1){
             if (pComponentPrivate->nUnhandledFillThisBuffers == 0)  {
                 pComponentPrivate->bFlushOutputPortCommandPending = OMX_FALSE;
-                /*pComponentPrivate->first_buff = 0;*/
+                pComponentPrivate->first_buff = 0;
                 if(pComponentPrivate->num_Op_Issued && !pComponentPrivate->reconfigOutputPort ){ //no buffers sent to DSP yet
                     aParam[0] = USN_STRMCMD_FLUSH;
                     aParam[1] = 0x1;
