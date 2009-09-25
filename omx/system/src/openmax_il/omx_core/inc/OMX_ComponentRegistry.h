a569385e5eb6a8ea55f1025acafdbd3015e3a55a
Christopher Kelly <c-kelly@ti.com>
2009-09-25 16:35:38 -0700

    Fix OMX_GetRolesOfComponents and OMX_GetComponentsOfRole, and clean up some unused code.
    Signed-off-by: Daniel Charles <dcharles@ti.com>
    
    Orignally from: https://partner.source.android.com/g/#change,1171
    Modified based on review feedbacks and comments

--- omx/system/src/openmax_il/omx_core/inc/OMX_ComponentRegistry.h
+++ omx/system/src/openmax_il/omx_core/inc/OMX_ComponentRegistry.h
@@ -1,12 +1,14 @@
 #include <dirent.h>
 
+/* macros */
 #define MAX_ROLES 20
 #define MAX_TABLE_SIZE 30
-
-/* limit the number of max occuring instances of same component,
-   tune this if you like */
 #define MAX_CONCURRENT_INSTANCES 1
+    /* limit the number of max occuring instances of same component,
+       tune this if you like
+    */
 
+/* struct definitions */
 typedef struct _ComponentTable {
     OMX_STRING name;
     OMX_U16 nRoles;
@@ -15,28 +17,6 @@ typedef struct _ComponentTable {
     int refCount;
 }ComponentTable;
 
-
-OMX_API OMX_ERRORTYPE TIOMX_GetRolesOfComponent ( 
-    OMX_IN      OMX_STRING compName, 
-    OMX_INOUT   OMX_U32 *pNumRoles,
-    OMX_OUT     OMX_U8 **roles);
-
-
+/* function prototypes */
 OMX_ERRORTYPE TIOMX_BuildComponentTable();
-OMX_ERRORTYPE ComponentTable_EventHandler(
-        OMX_IN OMX_HANDLETYPE hComponent,
-        OMX_IN OMX_PTR pAppData,
-        OMX_IN OMX_EVENTTYPE eEvent,
-        OMX_IN OMX_U32 nData1,
-        OMX_IN OMX_U32 nData2,
-        OMX_IN OMX_PTR pEventData);
 
-OMX_ERRORTYPE ComponentTable_EmptyBufferDone(
-        OMX_OUT OMX_HANDLETYPE hComponent,
-        OMX_OUT OMX_PTR pAppData,
-        OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
-
-OMX_ERRORTYPE ComponentTable_FillBufferDone(
-        OMX_OUT OMX_HANDLETYPE hComponent,
-        OMX_OUT OMX_PTR pAppData,
-        OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
