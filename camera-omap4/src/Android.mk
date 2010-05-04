f31b9760a6bcac63ddf14283ea9b4682bcc90dfa
Sundar Raman <sunds@ti.com>
2010-05-04 17:51:33 -0400

    OMAP4: New OMXCameraAdapter added and support for View Finder mode using Ducati
    This patch adds a new OMXCameraAdapter.so DLL for CameraHAL interaction with Ducati based OMXCamera
    and also adds support for viewfinding use-case with it.
    
    Following are supported:
    
    Camera UI app launch and view finder seen on the display
    Camera app close-relaunch for around 15 times
    
    Following are work in progress:
    Change of resolution (Currently, only NTSC and VGA resolutions work)
    Change of frame rates
    
    Following are known issues under debug:
    Preview freezes if left running for 10-12 seconds.
    Preview freezes after approximately 16 iterations of app close and relaunch.
    
    Change-Id: I4637e42aeaa45f4b1d9dcd280c71285c4647333f
    Signed-off-by: Sundar Raman <sunds@ti.com>

--- camera-omap4/src/Android.mk
+++ camera-omap4/src/Android.mk
@@ -100,7 +100,61 @@ include $(BUILD_SHARED_LIBRARY)
 
 ################################################
 
-#Insert OMX Camera Adapter in the future
+#OMX Camera Adapter
+
+include $(CLEAR_VARS)
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES:= \
+	BaseCameraAdapter.cpp \
+	OMXCameraAdapter/OMXCameraAdapter.cpp \
+
+
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/../inc/ \
+    $(LOCAL_PATH)/../inc/OMXCameraAdapter \
+    
+LOCAL_SHARED_LIBRARIES:= \
+    libdl \
+    libui \
+    libbinder \
+    libutils \
+    libcutils \
+    libtiutils \
+    libOMX_CoreOsal \
+    libOMX_Core \
+    libsysmgr \
+    librcm \
+    libipc \
+    libcamera \
+
+
+
+LOCAL_STATIC_LIBRARIES:= \
+	libipc_setup \
+	libomx_rpc \
+
+		
+
+LOCAL_C_INCLUDES += \
+    kernel/android-2.6.29/include \
+	frameworks/base/include/ui \
+	hardware/ti/omap3/liboverlay \
+	hardware/ti/omap3/libtiutils \
+	frameworks/base/include/utils \
+	hardware/ti/omx/ducati/domx/mmplatform \
+	hardware/ti/omx/ducati/domx/system/omx_core/inc \
+	hardware/ti/omx/ducati/domx/system/mm_osal/inc \
+	$(LOCAL_PATH)/../../../../../external/libxml2/include \
+
+
+
+LOCAL_CFLAGS += -fno-short-enums 
+
+LOCAL_MODULE:= libomxcameraadapter
+
+include $(BUILD_SHARED_LIBRARY)
 
 endif
 endif
