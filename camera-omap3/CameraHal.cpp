081bf671372b2612151e385859ca90051eb0179d
tluu <tluu@ti.com>
2010-07-28 19:14:33 -0500

    CameraHal OMAP3: Change 1MP dimensions so they are multiple of 8
    Conversions library currently only supports rotation with dimensions
    of multiples of 8.
    
    Change-Id: I71dc4b39de949c2cc7dc52652e6a59edb014b550

--- camera-omap3/CameraHal.cpp
+++ camera-omap3/CameraHal.cpp
@@ -68,7 +68,7 @@ typedef struct {
 
 int CameraHal::camera_device = 0;
 wp<CameraHardwareInterface> CameraHal::singleton;
-const char CameraHal::supportedPictureSizes [] = "3264x2448,2560x2048,2048x1536,1600x1200,1280x1024,1152x964,1280x960,800x600,640x480,320x240";
+const char CameraHal::supportedPictureSizes [] = "3264x2448,2560x2048,2048x1536,1600x1200,1280x1024,1152x968,1280x960,800x600,640x480,320x240";
 const char CameraHal::supportedPreviewSizes [] = "1280x720,992x560,864x480,800x480,720x576,720x480,768x576,640x480,320x240,352x288,176x144,128x96";
 const char CameraHal::supportedFPS [] = "33,30,25,24,20,15,10";
 const char CameraHal::supprotedThumbnailSizes []= "80x60";
@@ -76,7 +76,7 @@ const char CameraHal::PARAMS_DELIMITER []= ",";
 
 const supported_resolution CameraHal::supportedPictureRes[] = { {3264, 2448} , {2560, 2048} ,
                                                      {2048, 1536} , {1600, 1200} ,
-                                                     {1280, 1024} , {1152, 964} ,
+                                                     {1280, 1024} , {1152, 968} ,
                                                      {1280, 960} , {800, 600},
                                                      {640, 480}   , {320, 240} };
 
