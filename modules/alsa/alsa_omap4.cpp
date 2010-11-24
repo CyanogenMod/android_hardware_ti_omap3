2673cf6b23f26e8bb51face641d5f006936b6d86
shitiz <shitiz_kumar@ti.com>
2010-11-24 00:47:13 -0600

    Modified the mixer settings for BT SCO (HFP/HSP)
    Mixer settings for BT SCO(HF/HS) modified and moved inside if-else case for
    BT SCO MM use case.
    BT_VX_Switch is turned On always as previously becuase of ABE issue MM
    streaming on BT SCO headset is unstable. Moving the BT case inside
    if-else removes unstablitly for MM streaming case.
    
    Change-Id: Ia00f2d40571fd9cbe354262a9f42ab71b49cab16

--- modules/alsa/alsa_omap4.cpp
+++ modules/alsa/alsa_omap4.cpp
@@ -156,7 +156,7 @@ static alsa_handle_t _defaults[] = {
         sampleRate  : DEFAULT_SAMPLE_RATE,
         latency     : 200000, // Desired Delay in usec
         bufferSize  : DEFAULT_SAMPLE_RATE / 5, // Desired Number of samples
-        modPrivate  : (void *)&setScoControls,
+        modPrivate  : (void *)&setDefaultControls,
     },
     {
         module      : 0,
@@ -591,7 +591,7 @@ LOGV("%s", __FUNCTION__);
             control.set("DL1 Mixer Tones", 1);			// TONES_DL -> DL1 Mixer
             control.set("DL1 Mixer Voice", 1);			// VX_DL    -> DL1 Mixer
             control.set("Sidetone Mixer Playback", 1);		// DL1 Mixer-> Sidetone Mixer
-            control.set("DL1 PDM Switch", 1);			// Sidetone Mixer -> PDM_DL1
+            control.set("DL1 PDM Switch", 1);                 // Sidetone Mixer -> PDM_DL1
             control.set("DL1 Media Playback Volume", 118);
             control.set("DL1 Tones Playback Volume", 118);
             control.set("DL1 Voice Playback Volume", 118);
@@ -616,7 +616,19 @@ LOGV("%s", __FUNCTION__);
                 control.set("DL1 Capture Playback Volume", 0, -1);
                 control.set("DL2 Capture Playback Volume", 0, -1);
             }
-        } else {
+        } else if ((devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO) ||
+          (devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_HEADSET) ||
+          (devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_CARKIT)) {
+           /* OMAP4 ABE */
+           /* Bluetooth: DL1 Mixer */
+           LOGE("BT SCO MM");
+           control.set("DL1 Mixer Multimedia", 1);    // MM_DL    -> DL1 Mixer
+           control.set("Sidetone Mixer Playback", 1);           // DL1 Mixer-> Sidetone Mixer
+           control.set("DL1 BT_VX Switch", 1);                   // Sidetone Mixer -> BT-VX-D
+           control.set("DL1 Media Playback Volume", 118);
+           /* Disable McPDM */
+           control.set("DL1 PDM Switch", 0, 0);
+       } else {
             /* OMAP4 ABE */
             /* Headset: DL1 Mixer */
             control.set("DL1 Mixer Multimedia", 0, 0);
@@ -635,19 +647,6 @@ LOGV("%s", __FUNCTION__);
            /* FM Rx: DL1/DL2 Mixer */
             control.set("DL1 Capture Playback Volume", 0, -1);
             control.set("DL2 Capture Playback Volume", 0, -1);
-        }
-
-        if ((devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO) ||
-            (devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_HEADSET) ||
-            (devices & AudioSystem::DEVICE_OUT_BLUETOOTH_SCO_CARKIT)) {
-            /* OMAP4 ABE */
-            /* Bluetooth: DL1 Mixer */
-            control.set("DL1 Mixer Multimedia", 1);		// MM_DL    -> DL1 Mixer
-            control.set("Sidetone Mixer Playback", 1);		// DL1 Mixer-> Sidetone Mixer
-            control.set("DL1 BT_VX Switch", 1);			// Sidetone Mixer -> BT-VX-DL
-            control.set("DL1 Media Playback Volume", 118);
-        } else {
-            control.set("DL1 BT_VX Switch", 0, 0);
         }
     }
 
