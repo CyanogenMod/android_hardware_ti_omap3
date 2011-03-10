15c2746f6506d050c1a205b34d15df98284fb566
Homin Lee <homin.lee@ti.com>
2011-03-10 19:26:12 +0900

    TIHardwareRenderer: Fixed to make rotated overlay when it needed.
    Added argument rotationDegrees in TIHardwareRenderer::TIHardwareRenderer()
    for receive rotation degrees of video clip. and, unified -may havily be
    oveloaded- fucntions by gave default value to args.
    
    In this case, the rotated video not means images encoded in potrait size
    but, images be encoded as same as taken by camera sensor and have rotations
    info. in video track header.
    
    TIHardwareRenderer pass the degrees to liboverlay and the final image of
    video be rotated by power of overlay engine.
    
    Change-Id: Iba26fa61e6a8d2541b999d43318f61c0bb4c3e9b
    Signed-off-by: Homin Lee <homin.lee@ti.com>

--- libstagefrighthw/TIHardwareRenderer.h
+++ libstagefrighthw/TIHardwareRenderer.h
@@ -34,17 +34,14 @@ class Overlay;
 
 class TIHardwareRenderer : public VideoRenderer {
 public:
-    TIHardwareRenderer(
-            const sp<ISurface> &surface,
-            size_t displayWidth, size_t displayHeight,
-            size_t decodedWidth, size_t decodedHeight,
-            OMX_COLOR_FORMATTYPE colorFormat);
 //S3D
     TIHardwareRenderer(
             const sp<ISurface> &surface,
             size_t displayWidth, size_t displayHeight,
             size_t decodedWidth, size_t decodedHeight,
-            OMX_COLOR_FORMATTYPE colorFormat, int isS3D, int numOfOpBuffers = -1);
+            OMX_COLOR_FORMATTYPE colorFormat,
+            int32_t rotationDegrees = 0,
+            int isS3D = 0, int numOfOpBuffers = -1);
 
     virtual ~TIHardwareRenderer();
     void set_s3d_frame_layout(uint32_t s3d_mode, uint32_t s3d_fmt, uint32_t s3d_order, uint32_t s3d_subsampling);
--- libstagefrighthw/stagefright_overlay_output.cpp
+++ libstagefrighthw/stagefright_overlay_output.cpp
@@ -6,20 +6,62 @@ using android::sp;
 using android::ISurface;
 using android::VideoRenderer;
 
+VideoRenderer *createRendererWithRotation(
+        const sp<ISurface> &surface,
+        const char *componentName,
+        OMX_COLOR_FORMATTYPE colorFormat,
+        size_t displayWidth, size_t displayHeight,
+        size_t decodedWidth, size_t decodedHeight,
+        int32_t rotationDegrees) {
+    using android::TIHardwareRenderer;
+
+    TIHardwareRenderer *renderer =
+        new TIHardwareRenderer(
+                surface, displayWidth, displayHeight,
+                decodedWidth, decodedHeight,
+                colorFormat,
+                rotationDegrees);
+
+    if (renderer->initCheck() != android::OK) {
+        delete renderer;
+        renderer = NULL;
+    }
+
+    return renderer;
+}
+
+//remains for backward compatibility
 VideoRenderer *createRenderer(
         const sp<ISurface> &surface,
         const char *componentName,
         OMX_COLOR_FORMATTYPE colorFormat,
         size_t displayWidth, size_t displayHeight,
         size_t decodedWidth, size_t decodedHeight) {
+    return createRendererWithRotation(
+            surface, componentName, colorFormat,
+            displayWidth, displayHeight,
+            decodedWidth, decodedHeight,
+            0);
+}
+
+//S3D
+VideoRenderer *createRendererWithRotation(
+        const sp<ISurface> &surface,
+        const char *componentName,
+        OMX_COLOR_FORMATTYPE colorFormat,
+        size_t displayWidth, size_t displayHeight,
+        size_t decodedWidth, size_t decodedHeight,
+        int32_t rotationDegrees,
+        int isS3D, int numOfOpBuffers) {
     using android::TIHardwareRenderer;
 
     TIHardwareRenderer *renderer =
         new TIHardwareRenderer(
                 surface, displayWidth, displayHeight,
                 decodedWidth, decodedHeight,
-                colorFormat);
-
+                colorFormat,
+                rotationDegrees,
+                isS3D, numOfOpBuffers);
     if (renderer->initCheck() != android::OK) {
         delete renderer;
         renderer = NULL;
@@ -28,24 +70,17 @@ VideoRenderer *createRenderer(
     return renderer;
 }
 
-//S3D
+//remains for backward compatibility
 VideoRenderer *createRenderer(
         const sp<ISurface> &surface,
         const char *componentName,
         OMX_COLOR_FORMATTYPE colorFormat,
         size_t displayWidth, size_t displayHeight,
-        size_t decodedWidth, size_t decodedHeight, int isS3D, int numOfOpBuffers) {
-    using android::TIHardwareRenderer;
-    TIHardwareRenderer *renderer =
-        new TIHardwareRenderer(
-                surface, displayWidth, displayHeight,
-                decodedWidth, decodedHeight,
-                colorFormat, isS3D, numOfOpBuffers);
-    if (renderer->initCheck() != android::OK) {
-        delete renderer;
-        renderer = NULL;
-    }
-
-    return renderer;
+        size_t decodedWidth, size_t decodedHeight,
+        int isS3D, int numOfOpBuffers) {
+    return createRendererWithRotation(
+            surface, componentName, colorFormat,
+            displayWidth, displayHeight,
+            decodedWidth, decodedHeight,
+            0, isS3D, numOfOpBuffers);
 }
-
