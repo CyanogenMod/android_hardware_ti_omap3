c355703424efb47ee24233f57d71d258a87fb28d
Raghavendra Rayadurga <x0171697@ti.com>
2012-04-25 13:33:13 +0530

    OMAP3: Reduce max concurrent instances for 720P component to 1
    For OMAP3 ICS to fit in 512MB memory configuration, DMM pool size
    is reduced. This pool size is good enough to run only a single
    instance of 720p component but not 2.
    So reducing max concurrent instances to 1.
    
    Change-Id: I046bfeef4a560a31a3101821c24dc923573dc060
    Signed-off-by: Raghavendra Rayadurga <x0171697@ti.com>

--- omx/system/src/openmax_il/omx_core/inc/OMX_ComponentRegistry.h
+++ omx/system/src/openmax_il/omx_core/inc/OMX_ComponentRegistry.h
@@ -4,7 +4,7 @@
 #define MAX_ROLES 20
 #define MAX_TABLE_SIZE 30
 #define MAX_CONCURRENT_INSTANCES 4
-#define MAX_720P_CONCURRENT_INSTANCES 2
+#define MAX_720P_CONCURRENT_INSTANCES 1
     /* limit the number of max occuring instances of same component,
        tune this if you like
     */
