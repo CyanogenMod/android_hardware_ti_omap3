9c36bd81d967c431371c68410972382849ec8db8
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-26 22:22:10 +0000

    libstagefrighthw: Fix building with mr1
    MediaDebug.h is gone, pull in the macros we want
    
    Change-Id: Iafc69e7457746f978ff2497e612a0d48acd97e23

--- libstagefrighthw/TIOMXPlugin.cpp
+++ libstagefrighthw/TIOMXPlugin.cpp
@@ -19,7 +19,19 @@
 #include <dlfcn.h>
 
 #include <media/hardware/HardwareAPI.h>
-#include <media/stagefright/MediaDebug.h>
+
+#define LITERAL_TO_STRING_INTERNAL(x)    #x
+#define LITERAL_TO_STRING(x) LITERAL_TO_STRING_INTERNAL(x)
+
+#define CHECK_EQ(x,y)                                                   \
+    LOG_ALWAYS_FATAL_IF(                                                \
+            (x) != (y),                                                 \
+            __FILE__ ":" LITERAL_TO_STRING(__LINE__) " " #x " != " #y)
+
+#define CHECK(x)                                                        \
+    LOG_ALWAYS_FATAL_IF(                                                \
+            !(x),                                                       \
+            __FILE__ ":" LITERAL_TO_STRING(__LINE__) " " #x)
 
 namespace android {
 
