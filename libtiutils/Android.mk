9edbe7d34336e4ca3133b517400db46576f94e12
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-11-11 05:21:15 +0530

    libtiutils: fix include header path
    Change-Id: I8e0ffd23420af08a99f57011af98c2eca80a9d02

--- libtiutils/Android.mk
+++ libtiutils/Android.mk
@@ -19,7 +19,7 @@ LOCAL_SHARED_LIBRARIES:= \
     libcutils
 
 LOCAL_C_INCLUDES += \
-	frameworks/native/include/utils \
+	system/core/include/utils \
 	bionic/libc/include \
 	hardware/ti/omap4xxx/domx/omx_core/inc \
 	hardware/ti/omap4xxx/domx/mm_osal/inc
